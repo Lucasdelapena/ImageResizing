796d44248114ffffb8bfbf632751d083ffa7a95c
Lucasdelapena <lucas.delapena@outlook.com>
2024-09-05 22:11:51 -0500

    testing with python
    changing the project to python

---
+++ Project1CS4420.py
@@ -0,0 +1,17 @@
+import cv2
+
+# Load an image from file
+image = cv2.imread('test.jpg')  # Replace 'test_image.jpg' with your image file path
+
+# Check if the image was successfully loaded
+if image is None:
+    print("Error: Could not load image.")
+else:
+    print("Image loaded successfully.")
+
+    # Display the image in a window
+    cv2.imshow('Test Image', image)
+
+    # Wait for a key press and close the window
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
