65eeaa8ab9bef50c6731054e653faef4e48b5980
Lucasdelapena <lucas.delapena@outlook.com>
2024-09-06 22:14:38 -0500

    traversing through photos
    Got rid of the unnecessary gray scale stuff. Can traverse through the photos properly

--- Assignment1_CS4420.py
+++ Assignment1_CS4420.py
@@ -4,7 +4,6 @@ import numpy as np
 import random
 import os
 import argparse
-from pathlib import Path
 
 # f5 to run
 # python Assignment1_CS4420.py -rows 10 -cols 20 test.jpg
@@ -20,8 +19,6 @@ def main():
     cols = args.cols
 
     dir = args.dir
-    #dir_list = os.listdir(dir)
-    #print(dir_list)
     picslist = []
 
     for dirpath, dirnames, filenames in os.walk(dir, topdown=False): # gets directory path, directory names, and file names
@@ -29,9 +26,9 @@ def main():
             picslist.append(os.path.join(dirpath, filename))
             
     #print(picslist)
-
-
-    for pic in picslist:
+    imageNumber = 0
+    while True:
+        pic = picslist[imageNumber]
         # Read the image
         image = cv2.imread(pic)
 
@@ -42,16 +39,8 @@ def main():
         # Image dimensions
         print (f"Image dimensions: {image.shape[1]} x {image.shape[0]}")
 
-        # Read the same image as grayscale image
-        img_gray = cv2.imread(pic, cv2.IMREAD_GRAYSCALE)
-
-        # Display grayscale image
-        cv2.imshow('Grayscale Image', img_gray)
-        cv2.waitKey(0)
-
-        # Save a copy of grayscale image in a file on disk
-        gray_pic_file = os.path.splitext(pic)[0] + "_gray" + os.path.splitext(pic)[1]
-        cv2.imwrite(gray_pic_file, img_gray)
+        # Displayimage
+        cv2.imshow('Image', image)
 
         # Display value at a random pixel
         rows, cols, _ = image.shape
@@ -62,9 +51,19 @@ def main():
         pxl_color = image[r, c]
         print(f"Color pixel at ({r},{c}) = ({int(pxl_color[0])}, {int(pxl_color[1])}, {int(pxl_color[2])})")
 
-        # Grayscale pixel
-        pxl_gray = img_gray[r, c]
-        print(f"Grayscale pixel at ({r},{c}) = {int(pxl_gray)}")
+        useraction = cv2.waitKey(0) & 0xFF
+
+        if useraction == ord('n'):
+            if imageNumber < len(picslist) - 1:
+                imageNumber += 1
+        
+        if useraction == ord('p'):
+            if imageNumber > 0:
+                imageNumber -= 1
+
+        if useraction == ord('q'):
+            break
+
 
 
 if __name__ == "__main__":
